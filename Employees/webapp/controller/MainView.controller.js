c95893bcd10026953dab414df5579fc2c3782e95
aramlar <aramlar01@gmail.com>
2021-11-19 15:33:03 +0000

    Firts Commit 04_Elemen04_Elemento_Contr_UI5

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -5,11 +5,23 @@ sap.ui.define([
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
 	function (Controller) {
-		"use strict";
+        "use strict";
+      
+		var Main = Controller.extend("logaligroup.Employees.controller.MainView", {});
+		
+            Main.prototype.onValidate =  function () {
+                var inputEmployee = this.byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
 
-		return Controller.extend("logaligroup.Employees.controller.MainView", {
-			onInit: function () {
-
-			}
-		});
+                if (valueEmployee.length === 6){
+                  //inputEmployee.setDescription("OK");
+                  this.getView().byId("labelCountry").setVisible(true);
+                  this.getView().byId("slCountry").setVisible(true);
+                } else {
+                  //inputEmployee.setDescription("Not OK");
+                  this.getView().byId("labelCountry").setVisible(false);
+                  this.getView().byId("slCountry").setVisible(false);
+                };
+            }
+            return Main;
 	});
